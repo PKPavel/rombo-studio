ec0367e99ce2f82e1c85c5350bb6e3653203d63f
Pavel <petrykin412@gmail.com>
2026-05-15 01:49:33 +0300

    fix: update page.tsx with all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header'
+﻿import Header from './components/Header'
 import Hero from './components/Hero'
 import Archive from './components/Archive'
 import Founder from './components/Founder'
